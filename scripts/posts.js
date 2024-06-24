b213837e84440f4453c558df0e5421da0d9e4a5c
ALFI ALI <alfiali.333@gmail.com>
2024-06-24 15:52:13 -0700

    Done with my login and reg pages
    Done with my login and reg pages

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,3 +1,11 @@
 /* Posts Page JavaScript */
 
 "use strict";
+// const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
+fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts?limit=100&offset=0', {
+    headers: {
+      'accept': 'application/json',
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsZmlAbWQiLCJpYXQiOjE3MTkyNjgwMDcsImV4cCI6MTcxOTM1NDQwN30.Pb8ZUMHxkCj-GNOqsVF5mlXNIdIW6NVkGNAwQfR7PIk'
+    }
+  }); 
+  console.log(headers);
